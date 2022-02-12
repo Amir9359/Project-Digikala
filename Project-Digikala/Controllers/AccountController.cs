c17d79fbb44e86be322483e3272555b88c44d607
Amir9359 <52462695+Amir9359@users.noreply.github.com>
2022-02-12 11:26:06 +0330

    a12

---
+++ Project-Digikala/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_Digikala.Controllers
+{
+    public class AccountController : Controller
+{
+    public IActionResult Signin()
+    {
+        return View();
+    }
+        [HttpPost]
+    public IActionResult Signin( string username, string password,bool rememberme)
+    {
+        return View();
+    }
+    public IActionResult Signup()
+    {
+        return View();
+    }
+        [HttpPost]
+    public IActionResult Signup(string username, string password, string confirmpassword, string firstName,string lastName)
+    {
+        return View();
+    }
+}
+}
